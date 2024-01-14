31ed491e22474264416adc514a9021a4bded3ed7
Harshavardhan S U <141437035+Harshavardhan2099@users.noreply.github.com>
2024-01-14 16:37:51 +0530

    Source Code v1.2

--- script.js
+++ script.js
@@ -12,21 +12,51 @@ function closeNavBar()
     sideNavBar.style.left = "-60%"
 }
 
-var productContainer = document.getElementById('product')
+var menProductContainer = document.querySelector('.menProducts')
+var womenProductContainer = document.querySelector('.womenProducts')
+var kidProductContainer = document.querySelector('.kidProducts')
+
 var search = document.getElementById('search')
-var productList = productContainer.querySelectorAll('div')
+
+var menProductsList = menProductContainer.querySelectorAll('div')
+var womenProductsList = womenProductContainer.querySelectorAll('div')
+var kidProductsList = kidProductContainer.querySelectorAll('div')
+var infoList = document.querySelectorAll('.info')
+
 
 search.addEventListener('keyup', function(event){
     var enteredValue = event.target.value.toUpperCase()
-    for(var i=0; i<productList.length; i++)
+
+    for (let index = 0; index < infoList.length; index++) {
+        infoList[index].style.display = "none"        
+    }
+    for(var i=0; i<menProductsList.length; i++)
     {
-        var productName = productList[i].querySelector("p").textContent
-        if(productName.toUpperCase().indexOf(enteredValue) < 0)
+        var menProductName = menProductsList[i].querySelector("p").textContent
+        if(menProductName.toUpperCase().indexOf(enteredValue) < 0)
         {
-            productList[i].style.display = "none"
+         menProductsList[i].style.display = "none"
         }
         else{
-            productList[i].style.display = "block"
+         menProductsList[i].style.display = "block"
+        }
+
+        var womenProductName = menProductsList[i].querySelector("p").textContent
+        if(womenProductName.toUpperCase().indexOf(enteredValue) < 0)
+        {
+         womenProductsList[i].style.display = "none"
+        }
+        else{
+         womenProductsList[i].style.display = "block"
+        }
+
+        var kidProductName = kidProductsList[i].querySelector("p").textContent
+        if(kidProductName.toUpperCase().indexOf(enteredValue) < 0)
+        {
+         kidProductsList[i].style.display = "none"
+        }
+        else{
+         kidProductsList[i].style.display = "block"
         }
     }
 })
